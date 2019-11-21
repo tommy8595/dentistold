144f93c6f0cd1e2b5ca31a0a30c7301d7914863c
unknown <vuthyridhsam@gmail.com>
2019-11-21 18:46:55 +1100

    testing

--- dentist/dentist/frm_Expense.cs
+++ dentist/dentist/frm_Expense.cs
@@ -32,5 +32,36 @@ namespace dentist
         {
             Application.Run(new frm_Home());
         }
+        private void openAdd()
+        {
+            Application.Run(new frm_Expense_Add());
+        }
+        private void btn_Expense_Add_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Thread th = new Thread(openAdd);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+
+        private void btn_Expense_All_Click(object sender, EventArgs e)
+        {
+            dgv_Expense.DataSource = StoreProcedure.sp_get_expense("sd");
+        }
+
+        private void btn_Expense_Today_Click(object sender, EventArgs e)
+        {
+            dgv_Expense.DataSource = StoreProcedure.sp_get_expense("daily");
+        }
+
+        private void btn_Expense_Week_Click(object sender, EventArgs e)
+        {
+            dgv_Expense.DataSource = StoreProcedure.sp_get_expense("monthly");
+        }
+
+        private void btn_Expense_Year_Click(object sender, EventArgs e)
+        {
+            dgv_Expense.DataSource = StoreProcedure.sp_get_expense("yearly");
+        }
     }
 }
